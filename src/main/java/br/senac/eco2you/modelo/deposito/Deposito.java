16a86d7a47a216caa0e05da9f47e450c76ba3fb9
Fábio Alexandre Krug Wozniak <binho.krug.w@gmail.com>
2023-12-04 16:35:38 -0300

    Criada a classe Deposito

--- src/main/java/br/senac/eco2you/modelo/deposito/Deposito.java
+++ src/main/java/br/senac/eco2you/modelo/deposito/Deposito.java
@@ -1,5 +1,54 @@
 package br.senac.eco2you.modelo.deposito;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+ 
 public class Deposito {
-
+ 
+	private LocalDate data;
+	private Armazem armazem;
+	private Coletor coletor;
+	private List<ItemDeposito> itemDeposito;
+ 
+	public Deposito(LocalDate data, Armazem armazem, Coletor coletor) {
+		setData(data);
+		setArmazem(armazem);
+		setColetor(coletor);
+		itemDeposito = new ArrayList<>();
+ 
+	}
+ 
+	public Coletor getColetor() {
+		return coletor;
+	}
+ 
+	public void setColetor(Coletor coletor) {
+		this.coletor = coletor;
+	}
+ 
+	public Armazem getArmazem() {
+		return armazem;
+	}
+ 
+	public void setArmazem(Armazem armazem) {
+		this.armazem = armazem;
+	}
+ 
+	public LocalDate getData() {
+		return data;
+	}
+ 
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+ 
+	public List<ItemDeposito> getItemDeposito() {
+		return itemDeposito;
+	}
+ 
+	public void setItemDeposito(List<ItemDeposito> itemDeposito) {
+		this.itemDeposito = itemDeposito;
+	}
+ 
 }
