e4530a2d09c238cfe7b0a0042bb75f1ab3fce396
Eduardo Hessmann Rohleder <eduardo.h.rohleder@gmail.com>
2023-12-07 14:25:10 -0300

    Aterdado a lista de itens da retirada

--- src/main/java/br/senac/eco2you/modelo/retirada/Retirada.java
+++ src/main/java/br/senac/eco2you/modelo/retirada/Retirada.java
@@ -1,52 +1,63 @@
 package br.senac.eco2you.modelo.retirada;
-
+ 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
-
+ 
+import br.senac.eco2you.modelo.entidade.empresa.armazem.Armazem;
+import br.senac.eco2you.modelo.entidade.usuario.empresa.cooperativa.Cooperativa;
+import br.senac.eco2you.modelo.itemRetirada.ItemRetirada;
+ 
 public class Retirada {
-
+ 
 	private LocalDate data;
 	private Cooperativa cooperativa;
 	private Armazem armazem;
-	private List<ItemRetirada> listaItemRetiradas;
-
-	public Retirada(LocalDate data, Cooperativa cooperativa, Armazem armazem, List<ItemRetirada> listaItemRetiradas) {
+	private List<ItemRetirada> listaItensRetirada;
+ 
+	public Retirada(LocalDate data, Cooperativa cooperativa, Armazem armazem) {
 		this.data = data;
 		this.cooperativa = cooperativa;
 		this.armazem = armazem;
-		this.listaItemRetiradas = listaItemRetiradas;
+		listaItensRetirada = new ArrayList<>();
 	}
-
+ 
 	public LocalDate getData() {
 		return data;
 	}
-
+ 
 	public void setData(LocalDate data) {
 		this.data = data;
 	}
-
+ 
 	public Cooperativa getCooperativa() {
 		return cooperativa;
 	}
-
+ 
 	public void setCooperativa(Cooperativa cooperativa) {
 		this.cooperativa = cooperativa;
 	}
-
+ 
 	public Armazem getArmazem() {
 		return armazem;
 	}
-
+ 
 	public void setArmazem(Armazem armazem) {
 		this.armazem = armazem;
 	}
-
-	public List<ItemRetirada> getListaItemRetiradas() {
-		return listaItemRetiradas;
+ 
+	public List<ItemRetirada> getListaItensRetirada() {
+		return listaItensRetirada;
 	}
-
-	public void setListaItemRetiradas(List<ItemRetirada> listaItemRetiradas) {
-		this.listaItemRetiradas = listaItemRetiradas;
+ 
+	public boolean inserirItensRetirada(ItemRetirada itemRetirada) {
+		return listaItensRetirada.add(itemRetirada);
+ 
 	}
-
+ 
+	public boolean removerItensRetirada(ItemRetirada itemRetirada) {
+		return listaItensRetirada.remove(itemRetirada);
+ 
+	}
+ 
 }
