cb24fa31f6c4966fda8de2a45cf9f6944e89920b
FABIO ALEXANDRE KRUG WOZNIAK <fabio.wozniak@alunos.sc.senac.br>
2023-12-07 15:40:13 -0300

    Mapeamento classe ItemDeposito

--- src/main/java/br/senac/eco2you/modelo/entidade/itemDeposito/ItemDeposito.java
+++ src/main/java/br/senac/eco2you/modelo/entidade/itemDeposito/ItemDeposito.java
@@ -1,31 +1,48 @@
 package br.senac.eco2you.modelo.entidade.itemDeposito;
 
+import java.io.Serializable;
+
 import br.senac.eco2you.modelo.entidade.reciclavel.Reciclavel;
 
-public class ItemDeposito {
-	
+@Entity
+@Table(name = "item_deposito")
+public class ItemDeposito implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_reciclavel")
 	private Reciclavel reciclavel;
+
+	@Column(name = "quantidade_reciclaveis")
 	private int quantidadeReciclaveis;
-	 
-		public ItemDeposito(Reciclavel reciclavel, int quantidadeReciclaveis) {
-			setReciclavel(reciclavel);
-			setQuantidadeReciclaveis(quantidadeReciclaveis);;
-	 
-		}
-	 
-		public Reciclavel getReciclavel() {
-			return reciclavel;
-		}
-	 
-		public void setReciclavel(Reciclavel reciclavel) {
-			this.reciclavel = reciclavel;
-		}
-	 
-		public int getQuantidadeReciclaveis() {
-			return quantidadeReciclaveis;
-		}
-	 
-		public void setQuantidadeReciclaveis(int quantidadeReciclaveis) {
-			this.quantidadeReciclaveis = quantidadeReciclaveis;
-		}
+
+	  public ItemDeposito() {}
+	
+	public ItemDeposito(Reciclavel reciclavel, int quantidadeReciclaveis) {
+		setReciclavel(reciclavel);
+		setQuantidadeReciclaveis(quantidadeReciclaveis);
+		;
+
+	}
+
+	public Reciclavel getReciclavel() {
+		return reciclavel;
+	}
+
+	public void setReciclavel(Reciclavel reciclavel) {
+		this.reciclavel = reciclavel;
+	}
+
+	public int getQuantidadeReciclaveis() {
+		return quantidadeReciclaveis;
+	}
+
+	public void setQuantidadeReciclaveis(int quantidadeReciclaveis) {
+		this.quantidadeReciclaveis = quantidadeReciclaveis;
+	}
 }
