b2d26dcd899caf0846f06ae831307d0fedcd59c9
Eduardo Hessmann Rohleder <eduardo.h.rohleder@gmail.com>
2023-12-11 16:45:57 -0300

    Criado o DAO da classe Endereco

---
+++ src/main/java/br/senac/eco2you/modelo/dao/endereco/EnderecoDAOImpl.java
@@ -0,0 +1,172 @@
+package br.senac.eco2you.modelo.dao.endereco;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import br.senac.eco2you.modelo.entidade.endereco.Endereco;
+
+public class EnderecoDAOImpl implements EnderecoDAO {
+	
+	public void inserirEndereco(Endereco endereco) {
+
+		Session sessao = null;
+
+		try {
+
+			sessao = conectarBanco().openSession();
+			sessao.beginTransaction();
+
+			sessao.save(endereco);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+	}
+
+	public void deletarEndereco(Endereco endereco) {
+
+		Session sessao = null;
+
+		try {
+
+			sessao = conectarBanco().openSession();
+			sessao.beginTransaction();
+
+			sessao.delete(endereco);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+	}
+
+	public void atualizarEndereco(Endereco endereco) {
+
+		Session sessao = null;
+
+		try {
+
+			sessao = conectarBanco().openSession();
+			sessao.beginTransaction();
+
+			sessao.update(endereco);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+	}
+
+	public List<Endereco> recuperarEnderecos() {
+
+		Session sessao = null;
+		List<Endereco> clientes = null;
+
+		try {
+
+			sessao = conectarBanco().openSession();
+			sessao.beginTransaction();
+
+			CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+
+			CriteriaQuery<Endereco> criteria = construtor.createQuery(Endereco.class);
+			Root<Endereco> raizCliente = criteria.from(Endereco.class);
+
+			criteria.select(raizCliente);
+
+			clientes = sessao.createQuery(criteria).getResultList();
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+
+		return clientes;
+	}
+
+	private SessionFactory conectarBanco() {
+
+		Configuration configuracao = new Configuration();
+
+		configuracao.addAnnotatedClass(br.senac.eco2you.modelo.entidade.endereco.Endereco.class);
+		configuracao.addAnnotatedClass(br.senac.eco2you.modelo.entidade.deposito.Deposito.class);
+		configuracao.addAnnotatedClass(br.senac.eco2you.modelo.entidade.empresa.Empresa.class);
+		configuracao.addAnnotatedClass(br.senac.eco2you.modelo.entidade.itemDeposito.ItemDeposito.class);
+		configuracao.addAnnotatedClass(br.senac.eco2you.modelo.entidade.itemRetirada.ItemRetirada.class);
+		configuracao.addAnnotatedClass(exemplo.modelo.entidade.pedido.Pedido.class);
+		configuracao.addAnnotatedClass(br.senac.eco2you.modelo.entidade.endereco.Endereco.class);
+		configuracao.addAnnotatedClass(exemplo.modelo.entidade.endereco.Endereco.class);
+		configuracao.addAnnotatedClass(exemplo.modelo.entidade.pedido.Pedido.class);
+		configuracao.addAnnotatedClass(br.senac.eco2you.modelo.entidade.endereco.Endereco.class);
+		configuracao.addAnnotatedClass(exemplo.modelo.entidade.endereco.Endereco.class);
+		configuracao.addAnnotatedClass(exemplo.modelo.entidade.pedido.Pedido.class);
+		configuracao.addAnnotatedClass(br.senac.eco2you.modelo.entidade.endereco.Endereco.class);
+		configuracao.addAnnotatedClass(exemplo.modelo.entidade.endereco.Endereco.class);
+
+		configuracao.configure("hibernate.cfg.xml");
+
+		ServiceRegistry servico = new StandardServiceRegistryBuilder().applySettings(configuracao.getProperties()).build();
+		SessionFactory fabricaSessao = configuracao.buildSessionFactory(servico);
+
+		return fabricaSessao;
+	}
+
+}
