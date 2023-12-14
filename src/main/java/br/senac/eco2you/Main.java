5f951d54ba47b1723535248ee414affc87fa33d8
Daniel Mariano Marques <danielmarianomarquespr@gmail.com>
2023-12-14 15:10:25 -0300

    Alterado Main

--- src/main/java/br/senac/eco2you/Main.java
+++ src/main/java/br/senac/eco2you/Main.java
@@ -1,12 +1,20 @@
 package br.senac.eco2you;
 
+import br.senac.eco2you.modelo.dao.endereco.EnderecoDAO;
+import br.senac.eco2you.modelo.dao.endereco.EnderecoDAOImpl;
 import br.senac.eco2you.modelo.entidade.endereco.Endereco;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
-		Endereco endereco = new Endereco("89050-000", "Av. Brasil", 610, "SENAC", "(47) 3035-9999", "Ponta Aguda", "Blumenau", "Brasil", "0", "0");
-		
+
+		Endereco endereco = new Endereco("89050-000", "Av. Brasil", 610, "SENAC", "(47) 3035-9999", "Ponta Aguda",
+				"Blumenau", "Brasil", "0", "0");
+
+		EnderecoDAO enderecoDAO = new EnderecoDAOImpl();
+		enderecoDAO.inserirEndereco(endereco);
+
+		System.out.println(endereco.getBairro());
+
 	}
 }
