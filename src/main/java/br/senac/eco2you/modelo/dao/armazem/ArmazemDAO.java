897a814fb62a4c8ee739a2c9d88bd6b073b8e425
Eduardo Hessmann Rohleder <eduardo.h.rohleder@gmail.com>
2023-12-14 15:32:44 -0300

    Alterado o DAO e o mapeamento da calsse Armazem

--- src/main/java/br/senac/eco2you/modelo/dao/armazem/ArmazemDAO.java
+++ src/main/java/br/senac/eco2you/modelo/dao/armazem/ArmazemDAO.java
@@ -1,18 +1,10 @@
 package br.senac.eco2you.modelo.dao.armazem;
-
+ 
 import java.util.List;
 
 import br.senac.eco2you.modelo.entidade.usuario.empresa.armazem.Armazem;
-
+ 
 public interface ArmazemDAO {
-
-	void inserirArmazem(Armazem armazem);
-
-	void deletarArmazem(Armazem armazem);
-
-	void atualizarArmazem(Armazem armazem);
-	
-	Armazem buscarArmazemPorNome(String nome);
-	
+	List<Armazem> buscarArmazensPorNome(String nome);
 	List<Armazem> buscarArmazemPeloBairro(String bairro);
 }
--- src/main/java/br/senac/eco2you/modelo/dao/armazem/ArmazemDAOImpl.java
+++ src/main/java/br/senac/eco2you/modelo/dao/armazem/ArmazemDAOImpl.java
@@ -1,188 +1,96 @@
-//package br.senac.eco2you.modelo.dao.armazem;
-//
-//import java.util.List;
-//
-//import javax.persistence.criteria.CriteriaBuilder;
-//import javax.persistence.criteria.CriteriaQuery;
-//import javax.persistence.criteria.Join;
-//import javax.persistence.criteria.ParameterExpression;
-//import javax.persistence.criteria.Root;
-//
-//import org.hibernate.Session;
-//
-//import br.senac.eco2you.modelo.entidade.endereco.Endereco;
-//import br.senac.eco2you.modelo.entidade.endereco.Endereco_;
-//import br.senac.eco2you.modelo.entidade.usuario.empresa.armazem.Armazem;
-//import br.senac.eco2you.modelo.entidade.usuario.empresa.armazem.Armazem_;
-//import br.senac.eco2you.modelo.factory.conexao.ConexaoFactory;
-//
-//public class ArmazemDAOImpl implements ArmazemDAO{
-//	
-//	private ConexaoFactory fabrica;
-//	 
-//	public ArmazemDAOImpl() {
-//	fabrica = new ConexaoFactory();
-//	}
-//	
-//	public void inserirArmazem(Armazem armazem) {
-//		
-//		Session sessao = null;
-//
-//		try {
-//
-//			sessao = fabrica.getConexao().openSession();
-//			sessao.beginTransaction();
-//
-//			sessao.save(armazem);
-//
-//			sessao.getTransaction().commit();
-//
-//		} catch (Exception sqlException) {
-//
-//			sqlException.printStackTrace();
-//
-//			if (sessao.getTransaction() != null) {
-//				sessao.getTransaction().rollback();
-//			}
-//
-//		} finally {
-//
-//			if (sessao != null) {
-//				sessao.close();
-//			}
-//		}
-//		
-//	}
-//
-//	public void deletarArmazem(Armazem armazem) {
-//		
-//		Session sessao = null;
-//
-//		try {
-//
-//			sessao = fabrica.getConexao().openSession();
-//			sessao.beginTransaction();
-//
-//			sessao.delete(armazem);
-//
-//			sessao.getTransaction().commit();
-//
-//		} catch (Exception sqlException) {
-//
-//			sqlException.printStackTrace();
-//
-//			if (sessao.getTransaction() != null) {
-//				sessao.getTransaction().rollback();
-//			}
-//
-//		} finally {
-//
-//			if (sessao != null) {
-//				sessao.close();
-//			}
-//		}
-//		
-//	}
-//
-//	public void atualizarArmazem(Armazem armazem) {
-//		
-//		Session sessao = null;
-//
-//		try {
-//
-//			sessao = fabrica.getConexao().openSession();
-//			sessao.beginTransaction();
-//
-//			sessao.update(armazem);;
-//
-//			sessao.getTransaction().commit();
-//
-//		} catch (Exception sqlException) {
-//
-//			sqlException.printStackTrace();
-//
-//			if (sessao.getTransaction() != null) {
-//				sessao.getTransaction().rollback();
-//			}
-//
-//		} finally {
-//
-//			if (sessao != null) {
-//				sessao.close();
-//			}
-//		}
-//		
-//	}
-//
-//public Armazem buscarArmazemPorNome(String nome) {
-//	Session sessao = null;
-//	Armazem armazem = null;
-//
-//	try {
-//		sessao = fabrica.getConexao().openSession();
-//		sessao.beginTransaction();
-//
-//		CriteriaBuilder construtor = sessao.getCriteriaBuilder();
-//		CriteriaQuery<Armazem> criteria = construtor.createQuery(Armazem.class);
-//		Root<Armazem> raizCooperativa = criteria.from(Armazem.class);
-//
-//		criteria.select(raizCooperativa).where(construtor.like(raizCooperativa.get("nome"), "%" + nome + "%"));
-//
-//		return sessao.createQuery(criteria).uniqueResult();
-//
-//	} catch (Exception sqlException) {
-//
-//		sqlException.printStackTrace();
-//		if (sessao.getTransaction() != null) {
-//			sessao.getTransaction().rollback();
-//		}
-//
-//	} finally {
-//		if (sessao != null) {
-//			sessao.close();
-//		}
-//	}
-//
-//	return null;
-//}
-//
-//public List<Armazem> buscarArmazemPeloBairro(String bairro) {
-//	Session sessao = null;
-//	Armazem armazem = null;
-//
-//	try {
-//		sessao = fabrica.getConexao().openSession();
-//		sessao.beginTransaction();
-//
-//		CriteriaBuilder construtor = sessao.getCriteriaBuilder();
-//		CriteriaQuery<Armazem> criteria = construtor.createQuery(Armazem.class);
-//		Root<Armazem> raizCooperativa = criteria.from(Armazem.class);
-//
-//		Join<Armazem, Endereco> juncaoEndereco = raizCooperativa.join(Armazem_.endereco);
-//
-//		ParameterExpression<String> bairroEndereco = construtor.parameter(String.class);
-//		criteria.where(construtor.equal(juncaoEndereco.get(Endereco_.BAIRRO), bairroEndereco));
-//
-//		armazem = sessao.createQuery(criteria).setParameter(bairroEndereco, bairro);
-//
-//		
-//		sessao.getTransaction().commit();
-//
-//	} catch (Exception sqlException) {
-//
-//		sqlException.printStackTrace();
-//		if (sessao.getTransaction() != null) {
-//			sessao.getTransaction().rollback();
-//		}
-//
-//	} finally {
-//		if (sessao != null) {
-//			sessao.close();
-//		}
-//	}
-//
-//	return null;
-//
-//}
-//
-//}
+package br.senac.eco2you.modelo.dao.armazem;
+ 
+import java.util.ArrayList;
+import java.util.List;
+ 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
+ 
+import org.hibernate.Session;
+ 
+import br.senac.eco2you.modelo.entidade.endereco.Endereco;
+import br.senac.eco2you.modelo.entidade.endereco.Endereco_;
+import br.senac.eco2you.modelo.entidade.usuario.empresa.armazem.Armazem;
+import br.senac.eco2you.modelo.entidade.usuario.empresa.armazem.Armazem_;
+import br.senac.eco2you.modelo.factory.conexao.ConexaoFactory;
+ 
+public class ArmazemDAOImpl implements ArmazemDAO{
+	private ConexaoFactory fabrica;
+	public ArmazemDAOImpl() {
+	fabrica = new ConexaoFactory();
+	}
+
+ 
+public List<Armazem> buscarArmazensPorNome(String nome) {
+	Session sessao = null;
+	List<Armazem> armazens = new ArrayList<Armazem>();
+ 
+	try {
+		sessao = fabrica.getConexao().openSession();
+		sessao.beginTransaction();
+ 
+		CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+		CriteriaQuery<Armazem> criteria = construtor.createQuery(Armazem.class);
+		Root<Armazem> raizArmazem= criteria.from(Armazem.class);
+ 
+		criteria.select(raizArmazem).where(construtor.like(raizArmazem.get(nome), "%" + nome + "%"));
+		armazens = sessao.createQuery(criteria).getResultList();
+ 
+	} catch (Exception sqlException) {
+ 
+		sqlException.printStackTrace();
+		if (sessao.getTransaction() != null) {
+			sessao.getTransaction().rollback();
+		}
+ 
+	} finally {
+		if (sessao != null) {
+			sessao.close();
+		}
+	}
+	return armazens;
+ 
+	
+}
+ 
+public List<Armazem> buscarArmazemPeloBairro(String bairro) {
+	Session sessao = null;
+	List<Armazem> armazens = null;
+ 
+	try {
+		sessao = fabrica.getConexao().openSession();
+		sessao.beginTransaction();
+ 
+		CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+		CriteriaQuery<Armazem> criteria = construtor.createQuery(Armazem.class);
+		Root<Armazem> raizArmazem = criteria.from(Armazem.class);
+ 
+		Join<Armazem, Endereco> juncaoEndereco = raizArmazem.join(Armazem_.endereco);
+		ParameterExpression<String> bairroEndereco = construtor.parameter(String.class);
+		criteria.where(construtor.equal(juncaoEndereco.get(Endereco_.BAIRRO), bairroEndereco));
+		armazens = sessao.createQuery(criteria).setParameter(bairroEndereco, bairro).getResultList();
+ 
+		
+		sessao.getTransaction().commit();
+ 
+	} catch (Exception sqlException) {
+ 
+		sqlException.printStackTrace();
+		if (sessao.getTransaction() != null) {
+			sessao.getTransaction().rollback();
+		}
+ 
+	} finally {
+		if (sessao != null) {
+			sessao.close();
+		}
+	}
+ 
+	return armazens;
+ 
+}
+ 
+}
