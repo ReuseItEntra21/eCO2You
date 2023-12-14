0c5dc3c05b8a2b0b8722248e48a3c8bc366705d6
Daniel Mariano Marques <danielmarianomarquespr@gmail.com>
2023-12-14 14:59:03 -0300

    Criado Main

---
+++ src/main/java/br/senac/eco2you/Main.java
@@ -0,0 +1,12 @@
+package br.senac.eco2you;
+
+import br.senac.eco2you.modelo.entidade.endereco.Endereco;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Endereco endereco = new Endereco("89050-000", "Av. Brasil", 610, "SENAC", "(47) 3035-9999", "Ponta Aguda", "Blumenau", "Brasil", "0", "0");
+		
+	}
+}
