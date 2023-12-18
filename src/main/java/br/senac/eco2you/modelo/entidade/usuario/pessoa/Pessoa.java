e49b508c553478608f08324d044fbc1747c83c9f
HENRIQUE OLBRISCH SOARES <henrique.soares@alunos.sc.senac.br>
2023-12-18 14:14:14 -0300

    Corrigido erros nas classes Deposito, Retirada, Pessoa e Coletor

--- src/main/java/br/senac/eco2you/modelo/entidade/usuario/pessoa/Pessoa.java
+++ src/main/java/br/senac/eco2you/modelo/entidade/usuario/pessoa/Pessoa.java
@@ -1,35 +1,39 @@
 package br.senac.eco2you.modelo.entidade.usuario.pessoa;
-
+ 
 import java.io.Serializable;
 import java.sql.Date;
-
+ 
 import javax.persistence.Column;
-
+import javax.persistence.Entity;
+import javax.persistence.Table;
+ 
 import br.senac.eco2you.modelo.entidade.endereco.Endereco;
 import br.senac.eco2you.modelo.entidade.usuario.Usuario;
-
+ 
+@Entity
+@Table(name = "pessoa")
 public abstract class Pessoa extends Usuario implements Serializable {
-
+ 
 	private static final long serialVersionUID = 1L;
-
+ 
 	@Column(name = "sobrenome_pessoa", length = 25, nullable = false, unique = false)
 	private String sobrenome;
-
+ 
 	@Column(name = "apelido_pessoa", length = 10, nullable = true, unique = true)
 	private String apelido;
-
+ 
 	@Column(name = "idade_pessoa", nullable = false, unique = false)
 	private int idade;
-
+ 
 	@Column(name = "cpf_pessoa", length = 14, nullable = false, unique = true)
 	private String cpf;
-
+ 
 	@Column(name = "data_nascimento_usuario", nullable = false, unique = false)
 	private Date dataNascimento;
-
+ 
 	public Pessoa(String nome, String email, String senha, Endereco endereco, String sobrenome, String apelido,
 			int idade, String cpf, Date dataNascimento) {
-
+ 
 		super(nome, email, senha, endereco);
 		this.sobrenome = sobrenome;
 		this.apelido = apelido;
@@ -37,46 +41,45 @@ public abstract class Pessoa extends Usuario implements Serializable {
 		this.cpf = cpf;
 		this.dataNascimento = dataNascimento;
 	}
-
+ 
 	public Pessoa() {
 	}
-	
 	public String getSobrenome() {
 		return sobrenome;
 	}
-
+ 
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
-
+ 
 	public String getApelido() {
 		return apelido;
 	}
-
+ 
 	public void setApelido(String apelido) {
 		this.apelido = apelido;
 	}
-
+ 
 	public int getIdade() {
 		return idade;
 	}
-
+ 
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-
+ 
 	public String getCpf() {
 		return cpf;
 	}
-
+ 
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-
+ 
 	public Date getDatanascimento() {
 		return dataNascimento;
 	}
-
+ 
 	public void setDatanascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
