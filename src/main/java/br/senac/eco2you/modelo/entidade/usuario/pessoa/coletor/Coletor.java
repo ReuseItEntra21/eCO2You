2c0d8938c3253431495cc455012d31a07d49ec18
Daniel Mariano Marques <danielmarianomarquespr@gmail.com>
2023-12-18 15:15:37 -0300

    Correção de Erros nos MAIN

--- src/main/java/br/senac/eco2you/modelo/entidade/usuario/pessoa/coletor/Coletor.java
+++ src/main/java/br/senac/eco2you/modelo/entidade/usuario/pessoa/coletor/Coletor.java
@@ -18,23 +18,29 @@ import br.senac.eco2you.modelo.entidade.usuario.pessoa.Pessoa;
 @Entity
 @Table(name = "coletor")
 public class Coletor extends Pessoa implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
+	
 	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
 	@JoinTable(name = "coletor_deposito", joinColumns = @JoinColumn(name = "id_coletor"), inverseJoinColumns = @JoinColumn(name = "id_deposito"))
 	private List<Deposito> listaDepositos;
 
 	public Coletor(String nome, String sobrenome, String email, String senha, Endereco endereco, String apelido,
-			int idade, String cpf, Date dataNascimento) {
-		super(nome, email, senha, endereco, sobrenome, apelido, idade, cpf, dataNascimento);
+			int idade, String cpf, Date date) {
+		super(nome, email, senha, endereco, sobrenome, apelido, idade, cpf, date);
 		listaDepositos = new ArrayList<>();
 	}
+	
 	public Coletor() {}
+	
 	public List<Deposito> getListaDepositos() {
 		return listaDepositos;
 	}
+	
 	public boolean inserirDeposito(Deposito deposito) {
 		return listaDepositos.add(deposito);
 	}
+	
 	public boolean removerDeposito(Deposito deposito) {
 		return listaDepositos.remove(deposito);
 	}
