8c6ae208422ce6922f8f474f668bb25e535fecb8
Eduardo Hessmann Rohleder <eduardo.h.rohleder@gmail.com>
2023-12-21 17:27:54 -0300

    Retirado o atributo capacidade da classe cooperativa

--- src/main/java/br/senac/eco2you/modelo/entidade/usuario/empresa/cooperativa/Cooperativa.java
+++ src/main/java/br/senac/eco2you/modelo/entidade/usuario/empresa/cooperativa/Cooperativa.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
@@ -22,9 +21,6 @@ import br.senac.eco2you.modelo.entidade.usuario.empresa.Empresa;
 public class Cooperativa extends Empresa implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
-	@Column(name = "capacidade_retirada_cooperativa", length = 25, nullable = false)
-	private int capacidadeColeta;
 	
 	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
 	@JoinTable(name = "cooperativa_retirada", joinColumns = @JoinColumn(name = "id_cooperativa"), inverseJoinColumns = @JoinColumn(name = "id_retirada"))
@@ -32,18 +28,9 @@ public class Cooperativa extends Empresa implements Serializable {
 	
 	public Cooperativa () {}
 
-	public Cooperativa(String nome, String email, String senha, Endereco endereco, String cnpj, String horarioFuncionamento, int capacidadeColeta) {
+	public Cooperativa(String nome, String email, String senha, Endereco endereco, String cnpj, String horarioFuncionamento) {
 		super(nome, email, senha, endereco, cnpj, horarioFuncionamento);
-		setCapacidadeColeta(capacidadeColeta);
 		listaRetiradas = new ArrayList<>();
-	}
-
-	public int getCapacidadeColeta() {
-		return capacidadeColeta;
-	}
-
-	public void setCapacidadeColeta(int capacidadeColeta) {
-		this.capacidadeColeta = capacidadeColeta;
 	}
 
 	public List<Retirada> getListaRetiradas() {
