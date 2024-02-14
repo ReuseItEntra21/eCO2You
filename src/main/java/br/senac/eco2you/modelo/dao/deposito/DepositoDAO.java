acfc9a71e3837b1a4603c72b0f17eda3ab63eab6
Gabriel Fauxt <fauxtgabriel@gmail.com>
2024-02-14 17:20:21 -0300

    Corregido o DAO da classe deposito

--- src/main/java/br/senac/eco2you/modelo/dao/deposito/DepositoDAO.java
+++ src/main/java/br/senac/eco2you/modelo/dao/deposito/DepositoDAO.java
@@ -10,39 +10,39 @@ import br.senac.eco2you.modelo.enumeracao.status.deposito.StatusDeposito;
 
 public interface DepositoDAO {
 
- 
-    void inserirDeposito(Deposito deposito);
- 
-    void deletarDeposito(Deposito deposito);
- 
-    void atualizarDeposito(Deposito deposito);
- 
-    List<Deposito> buscarDepositos();
- 
-    List<Deposito> buscarDepositoPeloStatusDeposito(StatusDeposito statusDeDeposito);
- 
-    List<Deposito> buscarDepositoPelaData(LocalDate data);
-    
-    Deposito buscarDepositoPeloId(Long id);
- 
-    List<Deposito> buscarDepositoPeloArmazem(String nome);
-    
-    List<Deposito> buscarDepositoPeloArmazem(Armazem armazem);
- 
-    List<Deposito> buscarDepositoPeloArmazemEColetor(String nomeDoArmazem, String nomeDoColetor);
+	void inserirDeposito(Deposito deposito);
 
-	  List<Deposito> buscarDepositoPeloArmazemEColetorEStatus(String nomeDoArmazem, String nomeDoColetor, StatusDeposito status);
+	void deletarDeposito(Deposito deposito);
 
-	  List<Deposito> buscarDepositoPeloArmazemEColetorEData(String nomeDoColetor, String nomeDoArmazem, LocalDate data);
-  
-    List<Deposito> buscarDepositoPeloColetor(Coletor coletor);
-    
-    List<Deposito> buscarDepositoPeloColetorEArmazem(String nomeDoColetor, String nomeDoArmazem);
- 
-    List<Deposito> buscarDepositoPeloColetorEArmazemEStatus(String nomeDoColetor, String nomeDoArmazem, StatusDeposito status);
-    
-    List<Deposito> buscarDepositoPeloColetorEArmazemEData(String nomeDoColetor, String nomeDoArmazem, LocalDate data);
+	void atualizarDeposito(Deposito deposito);
 
+	List<Deposito> buscarDepositos();
+
+	List<Deposito> buscarDepositoPeloStatusDeposito(StatusDeposito statusDeDeposito);
+
+	List<Deposito> buscarDepositoPelaData(LocalDate data);
+
+	Deposito buscarDepositoPeloId(Long id);
+
+	List<Deposito> buscarDepositoPeloArmazem(String nome);
+
+	List<Deposito> buscarDepositoPeloArmazem(Armazem armazem);
+
+	List<Deposito> buscarDepositoPeloArmazemEColetor(String nomeDoArmazem, String nomeDoColetor);
+
+	List<Deposito> buscarDepositoPeloArmazemEColetorEStatus(String nomeDoArmazem, String nomeDoColetor,
+			StatusDeposito status);
+
+	List<Deposito> buscarDepositoPeloArmazemEColetorEData(String nomeDoColetor, String nomeDoArmazem, LocalDate data);
+
+	List<Deposito> buscarDepositoPeloColetor(Coletor coletor);
+
+	List<Deposito> buscarDepositoPeloColetorEArmazem(String nomeDoColetor, String nomeDoArmazem);
+
+	List<Deposito> buscarDepositoPeloColetorEArmazemEStatus(String nomeDoColetor, String nomeDoArmazem,
+			StatusDeposito status);
+
+	List<Deposito> buscarDepositoPeloColetorEArmazemEData(String nomeDoColetor, String nomeDoArmazem, LocalDate data);
+
+	public Deposito buscarDepositoComItemDepositoPorId(Long id);
 }
-
-
